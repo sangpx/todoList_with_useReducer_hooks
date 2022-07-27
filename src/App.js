ec6e2b5c41446b6b83ef50c60fce36e2c36364bf
sann <54765881+sangpx@users.noreply.github.com>
2022-07-27 18:06:19 +0700

    update hehe update hehe

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import "./styles.css";
+import { useReducer } from "react";
 
 // useReducer:
 // 1. init State:
 const initState = {
   job: "",
-  job: []
+  jobs: []
 };
 
 // 2. Actions:
+const SET_JOBS = "set_job";
+const ADD_JOBS = "add_job";
+const DELETE_JOBS = "delete_job";
 
-// 3. Reducer
+// 3. Reducers
+const reducer = (state, action) => {};
+
 // 4. Dispatch
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initState);
+  const { job, jobs } = state;
+
   return (
     <div className="App">
       <h2> Todo List App </h2>
-      <input placeholder="input job..." />
+      <input value={job} onChange={(e) => {}} placeholder="input job..." />
       <button> Add </button>
       <ul>
-        <li> rua bat &times; </li>
-        <li> nau com &times; </li>
-        <li> giat quan ao &times; </li>
+        {" "}
+        {jobs.map((job, index) => (
+          <li key={index}> {job} </li>
+        ))}{" "}
       </ul>
     </div>
   );
